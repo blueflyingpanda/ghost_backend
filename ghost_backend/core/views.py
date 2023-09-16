bb003a4ca33eebedf48b9aa95f0d1d2c38215b5c
blueflyingpanda <m.s.v.inkognito@gmail.com>
2023-09-16 12:32:02 +0300

    ref: project structure

---
+++ ghost_backend/core/views.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from rest_framework import permissions, viewsets
+
+from ghost_backend.core.serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+
+    queryset = User.objects.all().order_by("-date_joined")
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+def ping(request):
+    return HttpResponse('Pong')
